2ca22894d5994bb831ac025a73db5bd2765251df
Geoffrey Thompson <fleaplus@gmail.com>
2013-04-23 14:18:24 -0600

    Chapter 9 Playtime Complete

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -3,5 +3,11 @@ class StoreController < ApplicationController
   before_action :set_cart
   def index
     @products = Product.order(:title)
+    
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
   end
 end
