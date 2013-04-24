fb1f25abd34079ad140f13bebb54764e85aa9ead
Geoffrey Thompson <fleaplus@gmail.com>
2013-04-23 21:25:42 -0600

    WIP testing Cart.add_product

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+  fixtures :line_items
+  
+  test "cart line item should have quantity of one" do
+    cart = Cart.new
+    product = products(:ruby);
+    cart.add_product(product.id,product.price)
+    cart.add_product(product.id,product.price)
+    cart.save
+    
+    assert cart.line_items.size != 2, "number of line_items should NOT be 2"
+    assert cart.line_items.size == 1, "number of line_items should be 1"
+  end
+  
+  test "cart line item should have quantity of two" do
+    cart = Cart.new
+    cart.add_product(products(:one).id,products(:one).price)
+    cart.add_product(products(:two).id,products(:two).price)
+    cart.save
+    
+    assert cart.line_items.size == 2, "number of line_items should be 2"
+  end
+  
 end
